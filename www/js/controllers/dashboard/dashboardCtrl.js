d57073da55cadece1c709d478c8a5e2bef41dfa0
Anton Shevchenko <anton.shevchenko@osedea.com>
2015-03-26 08:43:33 -0400

    Minor design changes and addded refresher on dashboard view

--- www/js/controllers/dashboard/dashboardCtrl.js
+++ www/js/controllers/dashboard/dashboardCtrl.js
@@ -2,4 +2,13 @@ angular.module('app')
 
 .controller('DashboardCtrl', function($scope) {
 
+  // Get new data
+  $scope.getData = function() {
+    // For now, simply timeout
+    setTimeout(function() {
+      // Stop the ion-refresher from spinning
+      $scope.$broadcast('scroll.refreshComplete');
+    }, 1500);
+  };
+
 });
