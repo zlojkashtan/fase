405c020cd5c72ff25377065ab0ecac2bf694dd69
Anton Shevchenko <anton.shevchenko@osedea.com>
2015-03-26 08:45:53 -0400

    Fixed progress bar on bounds

--- www/js/directives/progressBar.js
+++ www/js/directives/progressBar.js
@@ -12,10 +12,10 @@ angular.module('app')
       var completed = angular.element($el[0].querySelector('.progress-bar-completed'));
       var bar = angular.element(completed.parent());
       var width = bar.prop('offsetWidth');
-
       var completedWidth = width * ($scope.value / $scope.max);
       var bounds = 22;
-      completed[0].style.width = (completedWidth ? completedWidth : bounds) + 'px';
+      
+      completed[0].style.width = (completedWidth ? completedWidth - bounds : bounds) + 'px';
     },
     templateUrl: 'templates/directives/progressBar.html'
   };
